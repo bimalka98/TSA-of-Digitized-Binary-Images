7f8517ff041e16020d0685a19254eaad427d32d2
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-01-28 18:03:19 +0530

    Timing analysis for findContour

--- MVbR2TG/MVbR2TG/libs/utilities/utilities.c
+++ MVbR2TG/MVbR2TG/libs/utilities/utilities.c
@@ -4,9 +4,11 @@ void showImage (int binaryimage[IMG_HEIGHT][IMG_WIDTH]) {
     printf ("\n");
     for ( int i = 0; i < IMG_HEIGHT;i++ ) {
         for ( int j = 0; j < IMG_WIDTH; j++ ) {
-            printf ("%d ", binaryimage[i][j]);
+            printf ("%d", binaryimage[i][j]);
         }
         printf ("\n");
     }
 }
+
+
 
