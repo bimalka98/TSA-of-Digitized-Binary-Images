1f4e801771007defdc9023bb2df2a4823ff81988
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-01-30 11:00:56 +0530

    Update gitignoree

--- MVbR2TG/MVbR2TG/src/main.c
+++ MVbR2TG/MVbR2TG/src/main.c
@@ -1,7 +1,13 @@
 /*
 Author: Bimalka Piyaruwan 
 Date Created: 2022/01/13
-Last Modified: 2022/01/27
+Last Modified: 2022/01/30
+
+Varable declaration convention
+Global Variables        : Start with capital, Combine with Capital letters
+Local Variables         : Start with underscore & a simple letter, combine with simple letters
+Function Arguments      : Start with simple letters, combine with simple letters
+#def define constants   : All Capitals and combined with underscore
 */
 
 // including libraries and dependancies
@@ -13,18 +19,6 @@ Last Modified: 2022/01/27
 #include "../libs/utilities/utilities.h" // Misscelaneous functions
 #include "../libs/tsa/tsa.h" // Paper: "Topological Structural Analysis of Digitized Binary Images by Border Following"
 #include "../libs/dsa/dsa.h" // Data structures and Algorithms
-//#include "tsa.h"
-
-/*
-Varable declaration convention
-Global Variables        : Start with capital, Combine with Capital letters
-Local Variables         : Start with underscore & a simple letter, combine with simple letters
-Function Arguments      : Start with simple letters, combine with simple letters
-#def define constants   : All Capitals and combined with underscore
-*/
-
-// global variable declarations
-
 #include <time.h>
 
 
@@ -39,44 +33,27 @@ int BinaryImage[IMG_HEIGHT][IMG_WIDTH] = {
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 */
 
-int BinaryImage[IMG_HEIGHT][IMG_WIDTH] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-        {0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} };
-
-
+// Sample binary images saved in header files: Remember to image size parameters in the tsa.h file
+#include "../data/raw/ImageHeaders/image256.h"     // BinaryImage256
+//#include "../data/raw/ImageHeaders/image512.h"   // BinaryImage512
+//#include "../data/raw/ImageHeaders/image1024.h"  // BinaryImage1024
+//#include "../data/raw/ImageHeaders/image2048.h"  // BinaryImage2048
+ 
 int main () {
 
-    // Represent the image
-
-    int image[IMG_HEIGHT][IMG_WIDTH];
-    for ( int i = 0; i < IMG_HEIGHT;i++ ) {
-        for ( int j = 0; j < IMG_WIDTH; j++ ) {
-            if ( (i > IMG_HEIGHT / 5) && (i < IMG_HEIGHT * 4 / 5) && (j >IMG_WIDTH / 5) && (j < IMG_WIDTH * 4 / 5) ) {
-                image[i][j] = 1;
-            } else {
-                image[i][j] = 0;
-            }
-        }
-    }
-    printf ("Image: %p", ( void* ) image);
     clock_t start, end;
     double cpu_time_used;
 
     start = clock (); 
-    struct Node* contours = findContours (image, IMG_WIDTH, IMG_HEIGHT);
+    struct Node* contours = findContours (BinaryImage256, IMG_WIDTH, IMG_HEIGHT);
     end = clock ();
     
     cpu_time_used = ( (( double ) ( end - start )) / CLOCKS_PER_SEC );
-    //printf ("\nExecution time: %f", cpu_time_used);
+    
     printf ("\nStart: %ld", start);
     printf ("\nEnd: %ld", end);
     printf ("\nExecution time: %f", cpu_time_used);
-    // Represent the image
-    //showImage (BinaryImage2);
+    
     
     return 0;
 }
---
+++ MVbR2TGpy/MVbR2TG/generateImages.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 30 09:52:13 2022
+
+@author: Bimalka Piyaruwan
+"""
+ImageSizes = [256, 512, 1024, 2048]
+path = 'E:\\MVbR2TG\\MVbR2TG\\MVbR2TG\\data\\raw\\ImageHeaders\\'
+
+for size in ImageSizes:
+    with open(path + 'image' + str(size)+ '.h', 'w') as imagefile:
+        imagefile.write('#include "../../libs/tsa/tsa.h"\n')
+        imagefile.write('int BinaryImage' + str(size) + '[IMG_HEIGHT][IMG_WIDTH] = {\n')
+        for i in range(size):
+            imagefile.write('\t{')
+            for j in range(size):
+                if ( (i > size / 5) and (i < size * 4 / 5) and (j >size / 5) and (j < size * 4 / 5) ):
+                    if j!=size-1:
+                        imagefile.write('1, ')
+                    else:
+                        imagefile.write('1')
+                
+                else:
+                    if j!=size-1:
+                        imagefile.write('0, ')
+                    else:
+                        imagefile.write('0')
+            if (i == size - 1):
+                imagefile.write('}\n')
+            else:
+                imagefile.write('},\n')  
+        imagefile.write('};\n')  
