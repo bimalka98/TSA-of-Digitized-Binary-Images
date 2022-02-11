5dac8ec48d9cb9ac7444089b7bab76616edd393e
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-02-11 21:41:30 +0530

    Add binary dummy images for timing analysis

--- MVbR2TGpy/MVbR2TG/generateImages.py
+++ MVbR2TGpy/MVbR2TG/generateImages.py
@@ -6,18 +6,23 @@ Created on Sun Jan 30 09:52:13 2022
 
 Script to generate test images for my findContour algorithm
 """
+import cv2 as cv
+import numpy as np
 
-ImageSizes = [16, 32,]# 64, 128, 256, 512, 1024, 2048]
-path = 'E:\\MVbR2TG\\MVbR2TG\\MVbR2TG\\data\\raw\\ImageHeaders\\'
+ImageSizes = [256, 512, 1024]
+path1 = "E:\\MVbR2TG\\MVbR2TG\\MVbR2TG\\data\\raw\\ImageHeaders\\"
+path2 = 'E:\\MVbR2TG\\MVbR2TG\\MVbR2TG\\data\\raw\\ImageFiles\\'
 
 for size in ImageSizes:
-    with open(path + 'image' + str(size)+ '.h', 'w') as imagefile:
+    Image = np.ones((size,size))*255
+    with open(path1 + 'image' + str(size)+ '.h', 'w') as imagefile:
         imagefile.write('#include "../../../libs/tsa/tsa.h"\n\n')
-        imagefile.write('int BinaryImage' + str(size) + '[IMG_HEIGHT][IMG_WIDTH] = {\n')
+        imagefile.write('short BinaryImage' + str(size) + '[IMG_HEIGHT][IMG_WIDTH] = {\n')
         for i in range(size):
             imagefile.write('\t{')
             for j in range(size):
                 if ( (i > size / 5) and (i < size * 4 / 5) and (j >size / 5) and (j < size * 4 / 5) ):
+                    Image[i][j] = 0 # for saving the image as a .png file
                     if j!=size-1:
                         imagefile.write('1, ')
                     else:
@@ -32,4 +37,5 @@ for size in ImageSizes:
                 imagefile.write('}\n')
             else:
                 imagefile.write('},\n')  
-        imagefile.write('};\n')  
+        imagefile.write('};\n')
+    cv.imwrite(path2 + 'image' + str(size) + '.png', Image) 
